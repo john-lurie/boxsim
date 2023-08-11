a463d5db382e324d11366bae602afa908142c23d
John C. Lurie <jlurie@protonmail.com>
2023-08-11 13:58:27 -0400

    Single particle reflects off corner

--- boxsim/simulation.py
+++ boxsim/simulation.py
@@ -5,11 +5,16 @@ import numpy as np
 def outer_subtract_self(array):
     """Compute the outer subtraction between an array and itself."""
     rank = array.ndim
-    
+
     if rank == 1:
         return array - array[:, np.newaxis]
     else:
         raise ValueError(f"Expected array of dimension 1, got: {rank}")
+
+
+def vector_magnitude(component_x, component_y):
+    """Return the vector magnitude given x- and y-components."""
+    return np.sqrt(component_x**2 + component_y**2)
 
 
 class Simulator:
@@ -23,7 +28,7 @@ class Simulator:
         self.pos_y = None
         self.vel_x = None
         self.vel_y = None
-        
+
         self.fig = None
         self.ax = None
         self.plotter = None
@@ -31,12 +36,12 @@ class Simulator:
     def make_particles(self, pos_vel=None):
         """
         Initialize the particles.
-        
+
         If positions and velocities are not specified, they will be chosen
         from a random uniform distribution.
         """
         msg = f"Expected 'pos_vel' to have shape (4, N), got: {pos_vel.shape}"
-        
+
         if pos_vel is not None:
             if pos_vel.ndim == 2 and pos_vel.shape[0] == 4:
                 self.pos_x = pos_vel[0]
@@ -49,19 +54,57 @@ class Simulator:
             # FUTURE: Draw from random uniform distribution.
             pass
 
-    def run(self):
-        """Run the simulation."""
+    def reflect_walls(self):
+        """Any particle outside of walls is reflected."""
+        # Logical not (~) makes inequality simpler.
+        outside_x = ~ (self.radius < self.pos_x < self.width - self.radius)
+        outside_y = ~ (self.radius < self.pos_y < self.width - self.radius)
+        self.vel_x[outside_x] *= -1
+        self.vel_y[outside_y] *= -1
+
+    def move_particles(self, timestep):
+        """Multiply velocities by a given timestep to update positions."""
+        self.pos_x += self.vel_x * timestep
+        self.pos_y += self.vel_y * timestep
+
+    def setup_plot(self, show=False):
+        """Setup the plotting window."""
         self.fig, self.ax = plt.subplots(dpi=120)
         self.ax.set_xlim(0.0, self.width)
         self.ax.set_ylim(0.0, self.width)
         self.ax.set_aspect(1)
+
+        # Set marker size to be in data units of current window.
+        # If window is resized, marker will not be in data units.
         self.fig.canvas.draw()
-        
-        window_ext = self.ax.get_window_extent().width
-        ms =  (self.radius / self.width) * window_ext * (72.0 / self.fig.dpi)
-        self.plotter, = self.ax.plot([], [], marker='o', ms=ms, mew=0,
-                                     color='k')
-        
-        self.plotter.set_data(self.pos_x, self.pos_y)
-        
-        plt.show()
+        window = self.ax.get_window_extent().width
+        ms =  (2 * self.radius / self.width) * window * (72 / self.fig.dpi)
+        self.plotter, = self.ax.plot(self.pos_x, self.pos_y, marker='o', ms=ms,
+                                     mew=0, color='k')
+        if show:
+            plt.show()
+
+    def run(self, duration=10.0, animate=False):
+        """Run the simulation."""
+        if animate:
+            plt.ion()
+
+        time = 0.0
+        while time <= duration:
+            speed = vector_magnitude(self.vel_x, self.vel_y)
+
+            if np.max(speed) == 0.0:
+                print("No particles are moving. Simulation over!")
+                break
+            else:
+                # Set timestep so that fastest particle moves 0.2 * radius.
+                # This will result in a long simulation if speed is low.
+                timestep = 0.2 * self.radius / np.max(speed)
+
+                if animate:
+                    self.plotter.set_data(self.pos_x, self.pos_y)
+                    plt.pause(timestep)
+
+                self.reflect_walls()
+                self.move_particles(timestep)
+                time += timestep
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -10,7 +10,7 @@ class TestSimulation(unittest.TestCase):
         self.simulator = simulation.Simulator()
 
     def test_simulator_init(self):
-        # Confirm default values.
+        """Confirm default values."""
         self.assertEqual(self.simulator.width, 100.0)
         self.assertEqual(self.simulator.radius, 1.0)
         pos_vel_tup = (self.simulator.pos_x, self.simulator.pos_y,
@@ -18,10 +18,10 @@ class TestSimulation(unittest.TestCase):
         self.assertEqual(pos_vel_tup, (None, None, None, None))
 
     def test_make_particles(self):
-        # Make a single particle.
+        """Make a single particle."""
         pos_vel = np.array([[1], [2], [3], [4]])
         self.assertEqual(pos_vel.shape, (4, 1))
-        self.simulator.make_particles(pos_vel)
+        self.simulator.make_particles(pos_vel.copy())
         self.assertEqual(self.simulator.pos_x, pos_vel[0])
         self.assertEqual(self.simulator.pos_y, pos_vel[1])
         self.assertEqual(self.simulator.vel_x, pos_vel[2])
@@ -30,24 +30,38 @@ class TestSimulation(unittest.TestCase):
         # Raises ValueError because array shape should be (4, N), not (1, 4).
         one_row = np.array([[0, 0, 0, 0]])
         self.assertEqual(one_row.shape, (1, 4))
-        with self.assertRaises(ValueError):        
+        with self.assertRaises(ValueError):
             self.simulator.make_particles(one_row)
 
-    def test_plotting(self):
-        self.simulator = simulation.Simulator(radius=3)
-        pos_vel = np.array([[50.0], [50.0], [2.0], [1.0]])
-        self.simulator.make_particles(pos_vel)
-        self.simulator.run()
+    def test_setup_plot(self):
+        """Confirm the marker size for particles is in data units."""
+        # As test, marker should have diameter equal to window width.
+        self.simulator = simulation.Simulator(radius=50)
+        pos_vel = np.array([[50.0], [50.0], [0.0], [0.0]])
+        self.simulator.make_particles(pos_vel.copy())
+        self.simulator.setup_plot(show=True)
+
+    def test_refect(self):
+        """Make sure a single particle reflects off the walls."""
+        self.simulator = simulation.Simulator(radius=4)
+        pos_vel = np.array([[50.0], [50.0], [10.0], [10.0]])
+        # copy is crucial, otherwise pos_vel changes with simulator
+        self.simulator.make_particles(pos_vel.copy())
+        self.simulator.setup_plot()
+        self.simulator.run(animate=True)
+        # Confirm particle now moving in opposite direction.
+        np.testing.assert_array_equal(self.simulator.vel_x, -1 * pos_vel[2])
+        np.testing.assert_array_equal(self.simulator.vel_y, -1 * pos_vel[3])
 
     def test_outer_subract_self(self):
         # Try passing an array of shape (1, 3).
         two_dim = np.array([[0, 0, 0]])
         with self.assertRaises(ValueError):
             difference_matrix = simulation.outer_subtract_self(two_dim)
-        
+
         array = np.array([1, 2, 3])
         difference_matrix = simulation.outer_subtract_self(array)
         expected_matrix = np.array([[ 0,  1,  2],
                                     [-1,  0,  1],
-                                    [-2, -1,  0]]) 
+                                    [-2, -1,  0]])
         np.testing.assert_array_equal(difference_matrix, expected_matrix)
